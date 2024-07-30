d362208fb1be054911d9b32a61018fe452cd4bdd
Adi-Ida404 <146981927+Adi-Ida404@users.noreply.github.com>
2024-07-30 16:55:13 +0000

    Finish reponsive finally

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,16 +8,11 @@ export const Sidebar = () => {
 
     return (
         <div className={`fixed top-0 left-0 h-full bg-slate-950 transition-transform transform lg:translate-x-0 lg:relative lg:w-3/12 lg:mr-2 lg:mt-4 rounded-lg z-50 ${isSidebarVisible ? 'translate-x-0' : '-translate-x-full'}`}>
-            <button onClick={hideSidebar} className="absolute top-2 right-2 text-white lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
             <ul className="rounded-sm bg-slate-900 text-slate-300 p-4">
                 <li className="p-2">
                     <img className="h-6 w-auto" src="https://imgs.search.brave.com/gAASQsi_SWZZJqpGrSlxBdrdObjjSOVwmUI09qMtkfg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9sb2dv/ZG93bmxvYWQub3Jn/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDE2/LzA5L3Nwb3RpZnkt/bG9nby1icmFuY2Et/d2hpdGUucG5n" alt="spotify-logo" />
                 </li>
-                <Link to="/" className="p-2 flex font-family-Raleway duration-200 ease-in font-semibold hover:text-slate-100 duration-500 hover:underline hover:underline-offset-4">
+                <Link onClick={hideSidebar} to="/" className="p-2 flex font-family-Raleway duration-200 ease-in font-semibold hover:text-slate-100 duration-500 hover:underline hover:underline-offset-4">
                     <svg className="h-6 w-auto icon glyph" fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="home-alt-2" stroke="#ffffff">
                         <g id="SVGRepo_iconCarrier">
                             <path d="M19.71,9.29l-7-7a1,1,0,0,0-1.42,0l-7,7A1,1,0,0,0,4,10V21a1,1,0,0,0,1,1H8a1,1,0,0,0,.54-.18,1,1,0,0,0,.17-.11,1,1,0,0,0,.11-.17A1,1,0,0,0,9,21V15a1,1,0,0,1,1-1h4a1,1,0,0,1,1,1v6a1,1,0,0,0,1,1h3a1,1,0,0,0,1-1V10A1,1,0,0,0,19.71,9.29Z"></path>
@@ -25,7 +20,7 @@ export const Sidebar = () => {
                     </svg>
                     <h4 className="ml-4">Home</h4>
                 </Link>
-                <Link to="/searchpage" className="p-2 flex font-family-Raleway duration-200 ease-in font-semibold hover:text-slate-100 duration-500 hover:underline hover:underline-offset-4">
+                <Link onClick={hideSidebar} to="/searchpage" className="p-2 flex font-family-Raleway duration-200 ease-in font-semibold hover:text-slate-100 duration-500 hover:underline hover:underline-offset-4">
                     <svg className="h-6 w-auto icon glyph" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round">
                         <circle cx="11" cy="11" r="8" />
                         <path d="m21 21-4.3-4.3" />
@@ -41,7 +36,7 @@ export const Sidebar = () => {
                         <path d="M8 8v12" />
                         <path d="M4 4v16" />
                     </svg>
-                    <Link to="/playlist">
+                    <Link onClick={hideSidebar} to="/playlist">
                         <h4 className="ml-4 duration-200 hover:text-slate-100 ease-in">Your Library</h4>
                     </Link>
                 </li>
